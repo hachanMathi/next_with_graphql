2f5b8b1085bcba2997e31d7dc5c5263f216bded2
hachanMathi <chandramathi.m@mitrahsoft.com>
2023-01-25 17:40:13 +0530

    -- Setup

---
+++ db/models/product.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+
+const { Schema } = mongoose
+
+mongoose.Promise = global.Promise
+
+const ProductsSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  productionCapacity: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+})
+
+ProductsSchema.index({ name: 'text' })
+
+module.exports =
+  mongoose.models.Product || mongoose.model('Product', ProductsSchema)
---
+++ db/schema.js
@@ -0,0 +1,32 @@
+import { gql } from 'apollo-server-micro'
+
+const typeDefs = gql`
+  # Products
+  type Product {
+    id: ID
+    name: String
+    productionCapacity: Int
+    price: Float
+    description: String
+  }
+
+  input ProductInput {
+    name: String!
+    productionCapacity: Int!
+    price: Float!
+    description: String
+  }
+
+  type Query {
+    getProducts: [Product]
+    getProduct(id: ID!): Product
+  }
+
+  type Mutation {
+    newProduct(input: ProductInput): Product
+    updateProduct(id: ID!, input: ProductInput): Product
+    deleteProduct(id: ID!): String
+  }
+`
+
+module.exports = typeDefs
